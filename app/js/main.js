73b82f3b2daa6eb5e669e9fdaf7b25b7aad49af6
Andrey Zinoviev <dronis_dronis@mail.ru>
2020-12-15 21:22:59 +0300

    настроить сборку

---
+++ app/js/main.js
@@ -0,0 +1,13 @@
+// const buttons = Array.from(document.querySelectorAll('.nav-element-link'));
+// buttons.forEach((button) => {
+//     button.addEventListener('click', (evt) => {
+//         buttons.forEach((button) => {
+//             button.classList.remove('nav-element-link_status_active');
+//             button.parentElement.querySelector('.nav-element-logo').classList.remove('nav-element-logo_status_active');
+//         });
+//         const elementLogo = evt.target.parentElement.querySelector('.nav-element-logo');
+//         elementLogo.classList.add('nav-element-logo_status_active');
+//         button.classList.add('nav-element-link_status_active');
+//     })
+    
+// })
